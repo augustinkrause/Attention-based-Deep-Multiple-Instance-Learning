12b0e90fc495ab671c8174995a0c9ce0dd7c856d
hassan1595 <hassan99hassan.hb@gmail.com>
2024-02-01 17:42:29 +0100

    predictions new

--- predict.py
+++ predict.py
@@ -5,28 +5,15 @@ import os
 from model.model import MILModel
 from model.model import MLIMNISTModel
 
-def confidence_measure(x, sigma):  #maps (.5,1) to (0,1)
-    if type(sigma) == int:
-        sigma = sigma              #control how much you penalize uncertainty
-        return 1/(1+math.exp(-sigma*(x-.75)))
-    else:
+def confidence_measure(x):  #maps (.5,1) to (0,1)
+
         return 2*x-1
 
 
 def predict(args):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    path = os.path.join("model/model_parameters",f"{args.dataset}_{args.mil_type}_{args.pooling_type}.pt")
-    load_model = torch.load(path, map_location=device)
-
-    if args.dataset in ["MUSK1", "MUSK2", "ELEPHANT", "TIGER", "FOX"]:
-        model = MILModel(args.dataset, args.mil_type, args.pooling_type)
-
-    elif args.dataset == "MNIST":
-        model = MLIMNISTModel(args.mil_type, args.pooling_type)
-
-    model.load_state_dict(load_model)
-    model.eval()
     
+    model = args.model
+      
     output = float(model(args.input)) #you can interpret this output as the probability that the bag is positive
     prediction = round(output)
 
@@ -35,34 +22,26 @@ def predict(args):
     else:
         confidence = output
 
-    return prediction, round(confidence_measure(confidence, args.sigma), ndigits=2)
+    return prediction, round(confidence_measure(confidence), ndigits=2)
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--input",
                         type = torch.Tensor)
-    parser.add_argument("--dataset", 
-                        choices=["MUSK1", "MUSK2", "ELEPHANT", "TIGER", "FOX", "MNIST"])
-    parser.add_argument("--mil-type", 
-                        choices=["embedding_based", "instance_based"])
-    parser.add_argument("--pooling-type", 
-                        choices=["max", "mean", "attention", "gated_attention"])
+    parser.add_argument("--model", 
+                        type = torch.Module)
     
     args = parser.parse_args()
     
     if args.input == None:
         raise ValueError("Specify an input sample.")
-    if args.dataset == None:
-        raise ValueError("Specify a dataset, i.e. MUSK1, MUSK2, ELEPHANT, TIGER, FOX OR MNIST.")
-    if args.mil_type == None:
-        raise ValueError("Specify the MIL type, i.e. embedding- or instance-based.")
-    if args.pooling_type == None:
-        raise ValueError("Specify a pooling type, i.e. max, mean, attention or gated attention.")
-
-
+    if args.model == None:
+        raise ValueError("Specify a model")
+ 
     prediction, confidence = predict(args)
     print(f"Prediction: {prediction}, Confidence: {confidence}")
+    print()
 
 if __name__ == "__main__":
     main()
--- run_prediction.py
+++ run_prediction.py
@@ -6,49 +6,55 @@ import sys
 import os
 from data.data import load_data
 
+from model.model import MILModel
+from model.model import MLIMNISTModel
+from data.data_utils.transformations import get_transformation
+
+
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--input",
-                        type = torch.Tensor)
-    parser.add_argument("--dataset", 
-                        choices=["MUSK1", "MUSK2", "ELEPHANT", "TIGER", "FOX", "MNIST"])
-    parser.add_argument("--mil-type", 
-                        choices=["embedding_based", "instance_based"])
-    parser.add_argument("--pooling-type", 
-                        choices=["max", "mean", "attention", "gated_attention"])
-    parser.add_argument("--sigma",
-                        choices=[.5,1,5,10,20])   
-    
     args = parser.parse_args()
     
-    for sigma in ["linear", 5, 10, 15 ,20]:
+    for threshold in [0.1, 0.2, 0.4, 0.6, 0.8, 0.9]:
 
-        output_folder = "data/predictions_new"
+        output_folder = "data/predictions"
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
-        path = os.path.join(output_folder, f"predictions{sigma}.txt")
+        path = os.path.join(output_folder, f"predictions_{threshold}.txt")
 
         with open(path, "w") as f:
             sys.stdout = f
 
             for dataset, mil_type, pooling_type in itertools.product(
-                    ["MUSK1", "MUSK2", "ELEPHANT", "TIGER", "FOX", "MNIST"],
+                    ["MUSK1", "MUSK2", "ELEPHANT", "TIGER", "FOX", "MNIST", "TCGA"],
                     ["embedding_based", "instance_based"],
                     ["max", "mean", "attention", "gated_attention"]
             ):
-                args.dataset = dataset
-                args.mil_type = mil_type
-                args.pooling_type = pooling_type
-                args.sigma = sigma
 
                 if mil_type == "instance_based" and pooling_type in ["attention", "gated_attention"]:
                     continue
+                
+                if dataset in ["MUSK1", "MUSK2", "ELEPHANT", "TIGER", "FOX", "TCGA"]:
+                    model = MILModel(dataset, mil_type, pooling_type)
+                    transformation = get_transformation(device)
+                elif dataset == "MNIST":
+                    model = MLIMNISTModel(mil_type, pooling_type,)
+                    transformation = get_transformation(device, normalization_params=255.)
 
+                if dataset == "TCGA":
+                    model_path = os.path.join("model/model_parameters",f"{dataset}_{mil_type}_{pooling_type}-seed1.pt")
+                else:
+                    model_path = os.path.join("model/model_parameters",f"{dataset}_{mil_type}_{pooling_type}.pt")
 
+                model.load_state_dict(torch.load(model_path))
+                model.to(device)
+                model.eval()
+                args.model = model
 
                 print(f"Dataset: {dataset}, MIL Type: {mil_type}, Pooling Type: {pooling_type}")
-                _, ds_eval = load_data(args.dataset)
+                _, ds_eval = load_data(dataset, transformation=transformation)
                 n_errors = 0
                 FN = []
                 FP = []
@@ -59,11 +65,11 @@ def main():
                 NCT = []
                 NCF = []
                 
-            
                 total = 0
                 counter = 0
+
                 for bag, label in ds_eval:
-                    args.input = torch.from_numpy(bag).to(dtype=torch.float32)
+                    args.input = bag
                     prediction, measure = predict(args)
                     total += measure
                     counter += 1
@@ -84,7 +90,7 @@ def main():
                             TP.append(measure)
                             #print(f"True Positive, Confidence: {measure}")    
 
-                    if measure > 0.5:
+                    if measure > threshold:
                         if prediction == label:
                             PCT.append(measure)
                         else:
@@ -122,7 +128,7 @@ def main():
                 print("Confidence predictions")
 
                 try:
-                    print("Confidence", (len(PCT) + len(PCF))/len(ds_eval))
+                    print("Confidence Ratio", (len(PCT) + len(PCF))/len(ds_eval))
                 except:
                     print("No Data")
 
